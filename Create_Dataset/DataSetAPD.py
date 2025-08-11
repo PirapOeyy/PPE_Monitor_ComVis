960924b5c7f5d08ac957087606b009f259ec3002
PirapOeyy <rafi.zahranf@gmail.com>
2025-08-11 09:38:46 +0700

    Train Commit

--- Create_Dataset/DataSetAPD.py
+++ Create_Dataset/DataSetAPD.py
@@ -2,16 +2,15 @@ import yaml
 import os
 import cv2
 from ultralytics import YOLO
-from skimage.io import imread
 
+#c:\ComVis_PPE_Monitor\DatasetTrain1_K3.yolov8
 data_yaml = {
-    'train' : 'D:\\1st Rafi UPI\\#MAGANG INDISMART\\PPE.dataset.yolov8\\train',
-    'val' : 'D:\\1st Rafi UPI\\#MAGANG INDISMART\\PPE.dataset.yolov8\\valid',
-    'nc' : 11,
-    'names' : ['Head', 'MaskRespi', 'NoHarness', 'Person', 'SafetyBoots', 'SafetyEarmuffs',
-               'SafetyGlasses', 'SafetyGloves', 'SafetyHarness', 'SafetyHelm', 'SafetyVest']
+    'train' : 'c:\\ComVis_PPE_Monitor\\DatasetTrain1_K3.yolov8\\train',
+    'val' : 'c:\\ComVis_PPE_Monitor\\DatasetTrain1_K3.yolov8\\valid',
+    'nc' : 6,
+    'names' :['Hardhat', 'No Hardhat', 'No Overall Suit', 'No Safety Boots', 'Overall Suit', 'Safety Boots']
 }
 
-with open('APD_K3.yaml', 'w') as file:
+with open('APD_Pertamina.yaml', 'w') as file:
     yaml.dump(data_yaml, file)
-print("File APD_K3.yaml berhasil dibuat.")
+print("File APD_Pertamina.yaml berhasil dibuat.")
--- Data_Training/dataTrain.py
+++ Data_Training/dataTrain.py
@@ -1,16 +1,23 @@
 from ultralytics import YOLO
 import os
 import yaml
+from multiprocessing import freeze_support
 
-model = YOLO('D:\\1st Rafi UPI\\#MAGANG INDISMART\\PPE_Monitoring\\Data_Training\\yolov8m.pt')
-model.train(
-    data='D:\\1st Rafi UPI\\#MAGANG INDISMART\\PPE_Monitoring\\Data_Training\\APD_K3.yaml',
-    epochs=50,
+def main():
+    model = YOLO("C:\\ComVis_PPE_Monitor\\PPE_Monitor_ComVis\\Data_Training\\yolov8m.pt")
+    model.train(
+    data="C:\\ComVis_PPE_Monitor\\PPE_Monitor_ComVis\\APD_Pertamina.yaml",
+    epochs=100,
     imgsz=640,
     batch=16,
-    device='cpu'
+    device=0
 )
 
-model.export(format='onnx', dynamic=True)
+    model.export(format='onnx', dynamic=True)
+
+if __name__ == '__main__':
+    freeze_support()
+    main()
+
 # Save the model
-model.save('D:\\1st Rafi UPI\\#MAGANG INDISMART\\PPE_Monitoring\\Data_Training\\yolo8m_apd_k3.onnx')
+#model.save('yolo8m_apd_k3.onnx')
